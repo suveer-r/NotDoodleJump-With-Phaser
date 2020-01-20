1d74122f4ca85be11ce73d11bf3e1ea33e7ac4f5
SuveerR <14318177+suveer-r@users.noreply.github.com>
2020-01-20 20:19:18 +0530

    Polished creation of player model and regular tiles. And the interaction of the two. Next step is to add logic to remove tiles when out of view. Add more on top as camera scrolls by. Add other tiles and coins and enemy units.

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
-var tile;
+// var tile;
 var scaleRatio = window.devicePixelRatio / 3;
 var player;
+var tilesGroup;
 var tn;
 var td;
 var tb;
@@ -10,113 +11,117 @@ var coin;
 var enemy_m;
 var enemy_s;
 class Game extends Phaser.Scene {
-    constructor() {
-        super('Game');
-    }
-    
-    preload (){
-        this.load.svg('player', 'assets/player-01.svg')
-        this.load.svg('tile', 'assets/')
-        this.load.svg('tile-n', 'assets/tile-n-01.svg')
-        this.load.svg('tile-d', 'assets/tile-d-01.svg')
-        this.load.svg('tile-b', 'assets/tile-b-01.svg')
-        this.load.svg('rocket', 'assets/')
-        this.load.svg('spring', 'assets/')
-        this.load.svg('coin', 'assets/')
-        this.load.svg('enemy-m', 'assets/')
-        this.load.svg('enemy-s', 'assets/')
-    }
+	constructor() {
+		super("Game");
+	}
 
-    
+	preload() {
+		/* Preload all assets. */
+		this.load.svg("player", "assets/player-01.svg");
+		this.load.svg("tile", "assets/");
+		this.load.svg("tile-n", "assets/tile-n-01.svg");
+		this.load.svg("tile-d", "assets/tile-d-01.svg");
+		this.load.svg("tile-b", "assets/tile-b-01.svg");
+		this.load.svg("rocket", "assets/");
+		this.load.svg("spring", "assets/");
+		this.load.svg("coin", "assets/");
+		this.load.svg("enemy-m", "assets/");
+		this.load.svg("enemy-s", "assets/");
+	}
 
-    create (){
-        /* AlignGrid util from https://phasergames.com/ */
-        this.aGrid=new AlignGrid({scene:this,rows:11, cols:11});
-        this.aGrid.showNumbers(); //debugging the Grid
+	create() {
+		/* AlignGrid util to debug from https://phasergames.com/ */
+		this.aGrid = new AlignGrid({ scene: this, rows: 11, cols: 11 });
+		this.aGrid.showNumbers(); //debugging the Grid
+		
+		/* Create Floor */
+		var floor = this.physics.add.image(game.config.width/2, 830,'tile-b');
+		floor.setImmovable();
+		floor.scale = 6;
 
-        // this.physics.world.setBounds(false, false, false, true);
-// scene.physics.world.setBounds(x, y, width, height, checkLeft, checkRight, checkUp, checkDown);
-        
+		/* Create Tiles/Platforms */
+		this.createTiles();
+		/* Create Player Model */
         this.createPlayer();
-        this.cameraYMin = 99999;
-        this.cameras.main.startFollow(player,true);
-        
 
-      /*   this.createTiles();
-        
+		/* Collision checks and events */
+		this.physics.add.collider(player, floor);
+		this.physics.add.collider(player, tilesGroup, this.bounceBack, null, this);
+		
+		/* Control setup, Kbd only for now! Mouse and touch or Gyro later */
+		this.key_left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+		this.key_right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+		this.key_Up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+	}
 
-         */
-        
-        /* tn = this.add.image(0,0,'tile-n');
-        this.aGrid.placeAtIndex(1,tn);
-        // tn.displayWidth=game.config.width*.2;
-        // tn.scaleY=tn.scaleX;
-        tn.scale = .5; */
+	/* Bounce off Regular Tiles */
+	bounceBack(_player, _tilesGroup){
+		if (_player.body.touching.down && _tilesGroup.body.touching.up)
+            {
+				player.body.velocity.y = -400;                
+            }
+	}
 
-        // this.placeTile(80,'tile-n');
-        // this.row = this.makeRow('tile-n','tile-d','tile-b');
-        // row.setImmovable();
-        // this.physics.add.collider(player, row);
+	/* Create Player Model */
+	createPlayer() {
+        player = this.physics.add.image(game.config.width/2, 3*game.config.height/4, "player");
+		// player.body.setCollideWorldBounds();
+		player.setVelocity(0, -400);
+		player.setGravityY(200);
+		player.setBounce(0.4);
+		player.body.checkCollision.up = false;
+		player.body.checkCollision.down = true;
+		player.body.checkCollision.left = false;
+		player.body.checkCollision.right = false;
+		player.scale = 0.5;
 
-        this.key_left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        this.key_right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        this.key_Up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-
+		player.yOrig = player.y;
+        player.yChange = 0;
     }
-    
-    
+	
+	/* Create Regular Tiles/Platform */
+    createTiles(){
+        tilesGroup = this.physics.add.staticGroup();
+        tilesGroup.enableBody = true;
+		
+        tilesGroup.createMultiple(20, 'tile-n');
 
-    createPlayer(){
-        player = this.physics.add.image(0,0, 'player');
-        this.aGrid.placeAtIndex(93,player);
-        player.body.setCollideWorldBounds();
-        player.setVelocity(0,-400);
-        player.setGravityY(200);
-        player.setBounce(.4);
-        player.body.checkCollision.up = false;
-        player.body.checkCollision.down = true;
-        player.body.checkCollision.left = false;
-        player.body.checkCollision.right = false;
-        // player.displayWidth=game.config.width*.1;
-        // player.scaleY=player.scaleX;
-        player.scale = .5;
-
-        /* this.player.yOrig = this.player.y;
-        this.player.yChange = 0; */
-
-    }
-    
-    /* controlPlayer(){
-        this.world.wrap( this.player, this.player.width / 2, false );
-        this.player.yChange = Math.max( this.player.yChange, Math.abs( this.player.y - this.player.yOrig ) );
-        
-        if( this.player.y > this.cameraYMin + this.game.height && this.player.alive ) {
-            // game over
+		// spawnTile();
+		for( var i = 0; i<5; i++){
+            this.spawnTile( Phaser.Math.Between( 25, this.physics.world.bounds.width - 25 ), this.physics.world.bounds.height - 200 * i );
         }
-        
-    }
-    */
-    
 
-    update (delta){
-        // this.platforms.forEach(this.wrapPlatform, this);
+	} 
+	/* Sub function for Regular tiles. Might be handy other tiles in future */  
+    spawnTile(x, y){
+	  var tile = tilesGroup.create(x, y, 'tile-n');
+	//   tile.scale = .5;
+        tile.setImmovable();
+		return tile;
+	}
 
-        if(this.key_left.isDown)
-            player.body.velocity.x = -400;
-        else if(this.key_right.isDown)
-            player.body.velocity.x = 400;
-            else
-                    player.body.velocity.x = 0;
-            
-        if(this.key_Up.isDown)
-            player.body.velocity.y = -400;
+	update(delta) {
 
-        this.physics.world.wrap(player, 32 );
+        /* Dynamically change world bounds based on player pos */
+		// scene.physics.world.setBounds(x, y, width, height, checkLeft, checkRight, checkUp, checkDown);
+        this.physics.world.setBounds(0, -player.yChange, this.physics.world.bounds.width, this.game.config.height + player.yChange);
 
-        /* if((player.y <= game.config.height/2) && (player.body.velocity.y < 0))
-            tile.setVelocityY(0-player.body.velocity.y); */
-    }
+        /* Camera tracking */
+        // this.cameras.main.startFollow(player, true);
+        this.cameras.main.setLerp(.5);
+        this.cameras.main.centerOnY(player.y);
+		
+		if (this.key_left.isDown) player.body.velocity.x = -400;
+		else if (this.key_right.isDown) player.body.velocity.x = 400;
+		else player.body.velocity.x = 0;
 
-    
+		if (this.key_Up.isDown) player.body.velocity.y = -400;
 
+		this.physics.world.wrap(player, player.width / 6, false);
+
+        /* track the maximum amount that the hero has travelled */
+		player.yChange = Math.max( player.yChange, Math.abs( player.y - player.yOrig ) );
+		
+	}
+	
 }
